6a473e53194f9cf98d365093b9fc447580e3a8a3
Max Shalom <max_shalom121@flatbush.org>
2017-08-23 18:09:39 -0400

    Add files via upload

---
+++ movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    // Use this for initialization - Happens once per start
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame - happens an unlimited amount of times
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x + 0.3f, gameObject.transform.position.y + 0);
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x - 0.3f, gameObject.transform.position.y + 0);
+
+        }
+    }
+}
---
+++ wheels.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wheels : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow)){
+            gameObject.transform.Rotate(new Vector3(10, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Rotate(new Vector3(-10, 0, 0));
+        }
+    }
+}
